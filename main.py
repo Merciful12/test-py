44012f9401b6a1f61fb2f88163a0d551ff1dee8a
Stanislav <future.12@yandex.ru>
2021-09-07 13:25:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ r = requests.get('http://worldclockapi.com/api/json/est/now')
 print(r.text)
 print(sys.argv)
 with open('index.html', 'w+') as f:
-  f.write('<h1>' + sys.argv[0] + '</h1>')
+  f.write('<h1>' + sys.argv[1] + '</h1>')
 
